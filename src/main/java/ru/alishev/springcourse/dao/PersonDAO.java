89465f94a979df92d0b18014cbeee359689c4a20
Banan4ikk <71829447+Banan4ikk@users.noreply.github.com>
2021-04-21 01:29:57 +0600

    first commit

---
+++ src/main/java/ru/alishev/springcourse/dao/PersonDAO.java
@@ -0,0 +1,46 @@
+package ru.alishev.springcourse.dao;
+
+import org.springframework.stereotype.Component;
+import ru.alishev.springcourse.controllers.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+@Component
+public class PersonDAO {
+    private static int peopleCount = 0;
+     private List<Person> people;
+    {
+        people = new ArrayList<>();
+
+        people.add(new Person(++peopleCount,"Tom","Tommy","Tom@gmail.com"));
+        people.add(new Person(++peopleCount,"Bob","Bobby","Bob@gmail.com"));
+        people.add(new Person(++peopleCount,"James","Jamesik","james@gmail.com"));
+        people.add(new Person(++peopleCount,"Cat","Catty","Cat@gmail.com"));
+    }
+
+    public List<Person> index(){
+        return people;
+    }
+
+    public Person show(int id){
+        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+    }
+
+    public void save(Person person) {
+        person.setId(++peopleCount);
+        people.add(person);
+    }
+
+    public void update(int id, Person updatedPerson) {
+        Person personToUpdate = show(id);
+
+        personToUpdate.setName(updatedPerson.getName());
+        personToUpdate.setSurname(updatedPerson.getSurname());
+        personToUpdate.setEmail(updatedPerson.getEmail());
+    }
+
+    public void delete(int id) {
+     people.removeIf( p-> p.getId()==id);
+    }
+}
